fe5c19b4abb92cfc44a3ff6651167ceb7fad755f
pprior <alupri5332@ieselcaminas.org>
2023-03-08 23:33:08 +0100

    ej3 T3

--- src/T3/ej2/phoneNumber.java
+++ src/T3/ej2/phoneNumber.java
@@ -3,7 +3,7 @@ package T3.ej2;
 import java.util.HashMap;
 import java.util.Map;
 
-import static Utilities.Util.readLine;
+import static Utilities.Util.*;
 
 /**
  * @author Pere Prior
@@ -12,17 +12,23 @@ import static Utilities.Util.readLine;
 public class phoneNumber {
 
     public static void main(String[] args) {
-        Map<String, Integer> phoneNumber = new HashMap<>();
-        phoneNumber.put("Manolo", 689326781);
-        phoneNumber.put("Paco", 634219883);
-        phoneNumber.put("Paca", 647938512);
+        phone.put("Manolo", 689326781);
+        phone.put("Paco", 634219883);
+        phone.put("Paca", 647938512);
 
+        phoneList(phone);
 
     }
 
     public static void phoneList(Map<String, Integer> phone){
         String contact = readLine("Enter the contact name:");
-        System.out.println();
+        if (phone.containsKey(contact)){
+            System.out.println(phone.get(contact));
+        } else {
+            System.out.println("This key doesn't exists");
+        }
     }
+
+    private static final Map<String, Integer> phone = new HashMap<>();
 
 }
---
+++ src/T3/ej3/phoneNumber2.java
@@ -0,0 +1,45 @@
+package T3.ej2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static Utilities.Util.*;
+
+/**
+ * @author Pere Prior
+ */
+
+public class phoneNumber2 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> manoloPhones = new ArrayList<>();
+        manoloPhones.add(689326781);
+        manoloPhones.add(938674524);
+        ArrayList<Integer> pacoPhones = new ArrayList<>();
+        pacoPhones.add(634219883);
+        ArrayList<Integer> pacaPhones = new ArrayList<>();
+        pacaPhones.add(647938512);
+        pacaPhones.add(947564721);
+        pacaPhones.add(910);
+
+        phone.put("Manolo", manoloPhones);
+        phone.put("Paco", pacoPhones);
+        phone.put("Paca", pacaPhones);
+
+        contactList(phone);
+
+    }
+
+    public static void contactList(Map<String, ArrayList<Integer>> phone){
+        String contact = readLine("Enter the contact name:");
+        if (phone.containsKey(contact)){
+            System.out.println(phone.get(contact));
+        } else {
+            System.out.println("This key doesn't exists");
+        }
+    }
+
+    private static final Map<String, ArrayList<Integer>> phone = new HashMap<>();
+
+}
