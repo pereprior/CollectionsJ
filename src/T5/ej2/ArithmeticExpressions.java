7cf9dd00101503bc28f3f5f8ba8923c2827edf4e
pprior <alupri5332@ieselcaminas.org>
2023-03-28 12:14:07 +0200

    t5.3

--- src/T5/ej2/ArithmeticExpressions.java
+++ src/T5/ej2/ArithmeticExpressions.java
@@ -1,5 +1,7 @@
 package T5.ej2;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -8,14 +10,17 @@ import java.util.Stack;
 public class ArithmeticExpressions {
 
     public static void main(String[] args) {
-        System.out.println(arithmeticStack("20 2 /"));
+        System.out.println(arithmeticStack("2 4 6 3 / * +"));
+        System.out.println(arithmeticStack("3 5 4 + *"));
+        System.out.println(arithmeticStack("1 1 - 8 /"));
+        System.out.println("---");
+        System.out.println(arithmeticQueue("2 4 6 3 / * +"));
+        System.out.println(arithmeticQueue("3 5 4 + *"));
+        System.out.println(arithmeticQueue("8 1 1 - /"));
     }
 
     public static int arithmeticStack(String message){
-        String operators = "+-*/";
         Stack<Integer> stack = new Stack<>();
-        String number = "";
-        int result;
 
         for (int i = 0; i<message.length(); i++){
             char c = message.charAt(i);
@@ -29,30 +34,21 @@ public class ArithmeticExpressions {
             if (operators.contains(String.valueOf(c))) {
                 switch(c) {
                     case '+':
-                        result=0;
-                        while (!stack.empty()) {
-                            result += stack.pop();
-                        }
+                        result = stack.pop() + stack.pop();
                         stack.push(result);
                         break;
                     case '-':
-                        int a = stack.pop();
-                        int b = stack.pop();
-                        result = b - a;
+                        result = stack.pop() - stack.pop();
                         stack.push(result);
                         break;
                     case '*':
-                        result=1;
-                        while (!stack.empty()) {
-                            result *= stack.pop();
-                        }
+                        result = stack.pop() * stack.pop();
                         stack.push(result);
                         break;
                     case '/':
-                        result = stack.pop();
-                        while (!stack.empty()) {
-                            result /= stack.pop();
-                        }
+                        int a = stack.pop();
+                        int b = stack.pop();
+                        result = b / a;
                         stack.push(result);
                         break;
                 }
@@ -60,5 +56,47 @@ public class ArithmeticExpressions {
         }
         return stack.pop();
     }
+
+    public static int arithmeticQueue(String message){
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < message.length(); i++){
+            char c = message.charAt(i);
+            if(Character.isDigit(c)){
+                number += c;
+            }
+            if(Character.isWhitespace(c) && !number.isEmpty()){
+                queue.add(Integer.parseInt(number));
+                number = "";
+            }
+            if (operators.contains(String.valueOf(c))) {
+                switch(c) {
+                    case '+':
+                        result = queue.poll() + queue.poll();
+                        queue.add(result);
+                        break;
+                    case '-':
+                        result = queue.poll() - queue.poll();
+                        queue.add(result);
+                        break;
+                    case '*':
+                        result = queue.poll() * queue.poll();
+                        queue.add(result);
+                        break;
+                    case '/':
+                        int a = queue.poll();
+                        int b = queue.poll();
+                        result = b / a;
+                        queue.add(result);
+                        break;
+                }
+            }
+        }
+        return queue.poll();
+    }
+
+    private static final String operators = "+-*/";
+    private static String number = "";
+    private static int result;
 
 }
